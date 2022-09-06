a9e3e73af7343b52d5c01ac98911cf829f0d578c
sandeepvarma8466 <svarma852@gmail.com>
2022-09-06 17:51:14 +0530

    LMS Bank details service

--- src/main/java/com/blz/bankdetailsservice/dto/BankDetailsDTO.java
+++ src/main/java/com/blz/bankdetailsservice/dto/BankDetailsDTO.java
@@ -3,19 +3,20 @@ package com.blz.bankdetailsservice.dto;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Data
 public class BankDetailsDTO {
-    @NotBlank(message = "accountnumber cannot be empty")
+    //@NotNull(message = "accountnumber cannot be empty")
     private long accountNo;
-    @NotBlank(message = "ifsc code cannot be empty")
+    //@NotNull(message = "ifsc code cannot be empty")
     private String ifscCode;
-    @Pattern(regexp = "^[A-Z][A-Za-z]{2,}$", message = "branch name invalid")
+    @Pattern(regexp = "^[A-Z][a-z]{2,}$", message = "branch name invalid")
     private String branch;
     @Pattern(regexp = "^[1-9]{2}\\s{1}[1-9]{1}[0-9]{9}$", message = "Invalid Mobile Number")
     private long linkedMobNo;
-    @Pattern(regexp = "^[A-Z][A-Za-z]{2,}$", message = "account holder name invalid")
+    @Pattern(regexp = "^[A-Z][a-z]{2,}$", message = "account holder name invalid")
     private String accountHolderName;
     @Pattern(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", message = "Invalid Email Id")
     private String emailId;
